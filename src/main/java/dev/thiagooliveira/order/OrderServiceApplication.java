31188a6b540d34df9eca68f64d18be6de8ce79bb
THIAGO OLIVEIRA <t.oliveira@kigroup.de>
2024-06-21 11:24:40 +0100

    added spring initializr with: spring web, spring data mongo db, spring for rabbitmq, lombok and testcontainers

---
+++ src/main/java/dev/thiagooliveira/order/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.thiagooliveira.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
